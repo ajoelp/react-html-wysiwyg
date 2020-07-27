4ee0bdab0b5983bc512fee1d29bdc42352db47fa
Joel Podrebarac <joelpodrebar@gmail.com>
2020-07-27 16:14:37 -0400

    Fix peerdeps

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
 import packageJson from './package.json';
