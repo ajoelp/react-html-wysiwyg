3a619017140383c11df73d22c62402cebbe7ad84
Joel Podrebarac <joelpodrebar@gmail.com>
2020-11-17 09:52:46 -0500

    Fix build

--- src/Dialogs/index.tsx
+++ src/Dialogs/index.tsx
@@ -1,9 +1,11 @@
-import { lazy } from 'react';
+import EmbedDialog from './EmbedDialog';
+import ImageDialog from './ImageDialog';
+import LinkDialog from './LinkDialog';
 
 const DIALOGS: { [key: string]: any } = {
-  embed: lazy(() => import('./EmbedDialog')),
-  image: lazy(() => import('./ImageDialog')),
-  link: lazy(() => import('./LinkDialog')),
+  embed: EmbedDialog,
+  image: ImageDialog,
+  link: LinkDialog,
 };
 
 export default DIALOGS;
--- tsdx.config.js
+++ tsdx.config.js
@@ -18,6 +18,7 @@ module.exports = {
         extract: !!options.writeMeta,
       })
     );
+    console.log(config.output);
     return config;
   },
 };
